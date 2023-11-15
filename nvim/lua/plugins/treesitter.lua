342a5a441ffe3cbfbf0f998f639230c36a8f9950
uomaep <uomaep@gmail.com>
2023-11-15 20:24:16 +0900

    feat: update

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,58 @@
+return {
+  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "sql",
+        "svelte",
+      },
+
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        line_events = { "BufWrite", "CursorHold" },
+      },
+
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25,
+        persist_queries = true,
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+    },
+
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
