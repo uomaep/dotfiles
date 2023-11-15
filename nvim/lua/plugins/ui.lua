9deb8b5eb603a62f15c171333d0f609ea72a6883
uomaep <uomaep@gmail.com>
2023-11-15 20:41:26 +0900

    fix: delete neovim scroll animation

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -60,17 +60,6 @@ return {
     },
   },
 
-  -- animations
-  {
-    "echasnovski/mini.animate",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.scroll = {
-        enable = false,
-      }
-    end,
-  },
-
   -- buffer line
   {
     "akinsho/bufferline.nvim",
