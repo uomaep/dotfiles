648c0c0ab3ce05961c3213b6a5ee586fcfbcce00
uomaep <uomaep@gmail.com>
2024-03-15 18:09:10 +0900

    Update the Neovim setup

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
-local discipline = require("uomaep.discipline")
+-- local discipline = require("uomaep.discipline")
 
-discipline.cowboy()
+-- discipline.cowboy()
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
