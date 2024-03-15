8998d19a28cabd8a227fb3de16724881de62e134
uomaep <uomaep@gmail.com>
2024-03-15 19:20:58 +0900

    Update the Neovim setup

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -61,15 +61,15 @@ return {
 	},
 
 	-- animations
-	{
-		"echasnovski/mini.animate",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.scroll = {
-				enable = false,
-			}
-		end,
-	},
+	-- {
+	-- 	"echasnovski/mini.animate",
+	-- 	event = "VeryLazy",
+	-- 	opts = function(_, opts)
+	-- 		opts.scroll = {
+	-- 			enable = false,
+	-- 		}
+	-- 	end,
+	-- },
 
 	-- buffer line
 	{
